555b3b1913a0997058b33d79765e23d9ca938d4a
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-03-28 10:26:54 +0200

    Setup project (spring initializer web+websocket+lombok)

---
+++ src/main/java/com/example/cgnjava231websocket/CgnJava231WebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.cgnjava231websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgnJava231WebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CgnJava231WebsocketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cgnjava231websocket/CgnJava231WebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cgnjava231websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgnJava231WebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
